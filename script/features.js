9884e6ce162b3a10057c50e646c38a5bd273b562
sosssoldi <angus_67@live.it>
2017-07-22 15:46:07 +0200

    Parte senza Login completata

---
+++ script/features.js
@@ -0,0 +1,25 @@
+$(function() {
+	var features = $('.info').length;
+
+	for(let i=0; i < features; i++) {
+
+		let link = 'a[href="#feature' + (i+1) + '"]';
+		let info = '#feature' + (i+1);
+
+		$(info).hide();
+		$(link).removeClass('hideMoreInfo');
+		$(link).addClass('showMoreInfo');
+		$(link).on('click', function() {
+			if($(info).is(':visible'))
+			{
+				$(info).hide();
+				$(link).html('Premi per saperne di più');
+			}
+			else
+			{
+				$(info).slideToggle(500);
+				$(link).html('Premi per nascondere');
+			}
+		});
+	}
+});
