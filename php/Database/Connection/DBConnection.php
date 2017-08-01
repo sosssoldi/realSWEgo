8591cfb01819c9415f0c02195759c2395686741f
mbonolo <bonolomarco@gmail.com>
2017-08-01 17:25:20 +0200

    delete old corrupted file

--- php/Database/Connection/DBConnection.php
+++
@@ -1,7 +0,0 @@
-<?php 
-	namespace php\Database\Connection;
-
-	interface DBConnection {
-		public function connect();
-	}
-?>
--- php/Object/Object.php
+++
@@ -1,6 +0,0 @@
-<?php 
-	namespace php\Object;
-
-	abstract class Object {
-	}
-?>
