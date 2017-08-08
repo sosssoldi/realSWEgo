8739317df62a513c3d5c3a31922af15f22cad2e3
ntintorri <tintorri.nicola@gmail.com>
2017-08-09 01:32:46 +0200

    Aggiunto footer e messaggi tracking e cambio password

--- contacts.php
+++ contacts.php
@@ -2,10 +2,14 @@
 	session_start();
 	
 	$page = file_get_contents("template/contacts.html");
-	if(empty($_SESSION))
+	if(empty($_SESSION)) {
 		$menu = file_get_contents("template/menuNoLogin.html");
-	else
+		$page = str_replace(':map:', 'mapVisitor.html', $page);
+	}
+	else {
 		$menu = file_get_contents("template/menuLogin.html");
+		$page = str_replace(':map:', 'mapLogin.html', $page);
+	}
 	$menu = str_replace('<li><a href="contacts.php">CONTATTI</a></li>', '<li class="here">CONTATTI</li>', $menu);
 	$page = str_replace(':menu:', $menu, $page);
 	echo $page;
